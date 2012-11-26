6e3975c08d9c8d38b8084ac1920e7495bca54d3c
Eason <jickyi521@gmail.com>
2012-11-26 18:10:06 +0800

    refine broadcast codes

--- src/com/pushyun/test/PushMsgIntentService.java
+++ src/com/pushyun/test/PushMsgIntentService.java
@@ -17,7 +17,7 @@ public class PushMsgIntentService extends MsgHandlerIntentService
     protected void onMessageDelivered(Context context, HashMap<String, String> msgMap)
     {
         mMsg = "***** onMessageDelivered *****message=" +msgMap.get("message");
-        PushGlobals.displayMessage(context, mMsg);
+        PushGlobals.sendPushBroadcast(context, PushGlobals.DISPLAY_MESSAGE_ACTION, mMsg);
         Log.v(LOG_TAG, mMsg);
 
     }
@@ -26,14 +26,14 @@ public class PushMsgIntentService extends MsgHandlerIntentService
     protected void onError(Context context, String error)
     {
         mMsg = "***** onError *****" +error; 
-        PushGlobals.displayMessage(context, mMsg);
+        PushGlobals.sendPushBroadcast(context, PushGlobals.DISPLAY_MESSAGE_ACTION, mMsg);
         Log.v(LOG_TAG, mMsg);
     }
 
     @Override
     protected void onRegistered(Context context, String regId, boolean inGCM)
     {
-        PushGlobals.displayMessage(context, "Registration Id = " +regId);
+        PushGlobals.sendPushBroadcast(context, PushGlobals.DISPLAY_MESSAGE_ACTION, "Registration Id = " +regId);
         Log.v(LOG_TAG, "Register in GCM" + inGCM + "***** onRegistered *****" + regId);
     }
 
